2924b86cf9f100699cbddd23071269a7689f936e
Antarip Chatterjee <85601268+AntaripChat@users.noreply.github.com>
2023-03-31 22:58:51 +0530

    Create middl.js

---
+++ middl.js
@@ -0,0 +1,47 @@
+const theatre = require('../model/theatre.model');
+function validatetheatreReqBody(req, res, next){
+    if(!req.body.name){
+        return res.status(400).send({
+            msg: 'name field is missing in  creation'
+        })
+    }
+
+    if(!req.body.description){
+        return res.status(400).send({
+            msg: 'description field is missing in theatre creation'
+        })
+    }
+
+    if(!req.body.city){
+        return res.status(400).send({
+            msg: 'releaseStatus field is missing in theatre creation'
+        })
+    };
+  
+    let city = await User.findOne({city:req.body.city});
+    
+    if(city != null){
+        return res.status(400).send({message:"city already exists"});
+    };
+
+    if (!req.body.pin) {
+            return res.status(400).send({
+                message: "Failed! pin is not provided !"
+            });
+    
+    };
+  let pin = await User.findOne({pin:req.body.pin});
+    
+    if(pin != null){
+        return res.status(400).send({message:"pin already exists"});
+    };
+
+    next();
+
+}
+
+
+
+module.exports = {
+    validatetheatreReqBody
+}
